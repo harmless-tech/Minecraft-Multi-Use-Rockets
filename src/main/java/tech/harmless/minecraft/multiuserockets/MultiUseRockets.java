4482309aecbb0a014dbe4c8d917ab338c8931517
Harmless_Tech <2146578-harmless_tech@users.noreply.gitlab.com>
2020-12-26 21:03:11 -0500

    Initial Commit - MUR

---
+++ src/main/java/tech/harmless/minecraft/multiuserockets/MultiUseRockets.java
@@ -0,0 +1,10 @@
+package tech.harmless.minecraft.multiuserockets;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MultiUseRockets implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
