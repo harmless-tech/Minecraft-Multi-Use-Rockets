94d109b08dd8bbcbce326682db0c39a4d0329297
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2020-12-27 23:17:40 -0500

    Improved build.gradle file

--- src/main/java/tech/harmless/minecraft/multiuserockets/MultiUseRockets.java
+++ src/main/java/tech/harmless/minecraft/multiuserockets/MultiUseRockets.java
@@ -1,11 +1,21 @@
 package tech.harmless.minecraft.multiuserockets;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tech.harmless.minecraft.htlib.lib.annotations.HTMod;
 
 //TODO Use HTLib.
-public class MultiUseRockets /*implements ModInitializer*/ {
+@HTMod(id = MultiUseRockets.ID, name = MultiUseRockets.NAME, version = MultiUseRockets.VERSION)
+public class MultiUseRockets implements ModInitializer {
 
-    /*@Override
+    public static final String NAME = "MultiUseRockets";
+    public static final String ID = "multiuserockets";
+    public static final String VERSION = "0.0.1";
+
+    public static final Logger LOG = LogManager.getLogger();
+
+    @Override
     public void onInitialize() {
-    }*/
+    }
 }
