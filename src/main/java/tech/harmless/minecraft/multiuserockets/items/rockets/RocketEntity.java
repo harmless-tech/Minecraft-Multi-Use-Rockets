1bfc54c31adbcafb2afb45ca8a614242f6ee1620
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2021-01-01 21:09:36 -0500

    Started on fireworks

--- src/main/java/tech/harmless/minecraft/multiuserockets/items/rockets/RocketEntity.java
+++ src/main/java/tech/harmless/minecraft/multiuserockets/items/rockets/RocketEntity.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.OptionalInt;
 
+//TODO Use nbt or something else?
 public class RocketEntity extends FireworkRocketEntity {
 
     public RocketEntity(@NotNull World world) {
@@ -16,10 +17,23 @@ public class RocketEntity extends FireworkRocketEntity {
     }
 
     public RocketEntity(@NotNull World world, @NotNull ItemStack stack, @NotNull LivingEntity shooter) {
-        super(world, stack, shooter);
-        //this(world, shooter, shooter.getX(), shooter.getY(), shooter.getZ(), stack);
-        //this.dataTracker.set(SHOOTER_ENTITY_ID, OptionalInt.of(shooter.getEntityId()));
-        //this.shooter = shooter;
-        super.shooter = null;
+        super(EntityType.FIREWORK_ROCKET, world);
+
+        dataTracker.set(SHOOTER_ENTITY_ID, OptionalInt.of(shooter.getEntityId()));
+        this.shooter = shooter;
+
+        setOwner(shooter);
+
+        life = 0;
+        updatePosition(shooter.getX(), shooter.getY(), shooter.getZ());
+
+        int i = 1;
+        if(!stack.isEmpty() && stack.hasTag()) {
+            dataTracker.set(ITEM, stack.copy());
+            i += stack.getOrCreateSubTag("Fireworks").getByte("Flight"); //TODO Different tag?
+        }
+
+        setVelocity(this.random.nextGaussian() * 0.001D, 0.05D, this.random.nextGaussian() * 0.001D);
+        lifeTime = 10 * i + this.random.nextInt(6) + this.random.nextInt(7); //TODO Change.
     }
 }
