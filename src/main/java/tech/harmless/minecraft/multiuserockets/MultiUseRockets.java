444565e4482e8d611a02de9d50519954cae2ea33
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2022-05-17 22:41:18 -0400

    Version change

--- src/main/java/tech/harmless/minecraft/multiuserockets/MultiUseRockets.java
+++ src/main/java/tech/harmless/minecraft/multiuserockets/MultiUseRockets.java
@@ -15,7 +15,7 @@ public class MultiUseRockets implements ModInitializer {
 
     public static final String NAME = "MultiUseRockets";
     public static final String ID = "multiuserockets";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.1.0";
 
     public static final Logger LOG = LogManager.getLogger();
 
